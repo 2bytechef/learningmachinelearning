59c48b599fb1a7ad1cc0b3d79716a1560d2e0657
2bytechef <wilhiteross@gmail.com>
2024-12-23 01:15:35 -0800

    some more threading work

--- common/thread_cafe.py
+++ common/thread_cafe.py
@@ -1,5 +1,6 @@
 
 import threading
+from collections import defaultdict
 
 from common.jobs import Job
 
@@ -8,47 +9,53 @@ class ThreadCafe:
     class ThreadCounter:
         def __init__(self):
             self.queue_condition = threading.Condition()
-            self._thought_thread_count = 0
-            self._action_thread_count = 0
-            self._notification_thread_count = 0
+            self._thread_counts = defaultdict(lambda: 0)
+            self._thread_stops = defaultdict(lambda: threading.Event())
+    
+        @property
+        def compatible_job_types(self):
+            return [Job.JobType.THOUGHT, Job.JobType.ACTION, Job.JobType.NOTIFICATION]
             
-        def add_thread(self, job_type: Job.JobType):
+        def add_thread(self, job: Job):
             with self.queue_condition:
-                if job_type == Job.JobType.THOUGHT:
-                    self._thought_thread_count += 1
-                elif job_type == Job.JobType.ACTION:
-                    self._action_thread_count += 1
-                elif job_type == Job.JobType.NOTIFICATION:
-                    self._notification_thread_count += 1
+                if job.type in self.compatible_job_types:
+                    self._thread_counts[job.type] += 1
                 else:
-                    raise ValueError("Invalid JobType")
+                    raise ValueError(f"Invalid `{job.type.__class__.__name__}`")
             
-        def remove_thread(self, job_type: Job.JobType):
+        def remove_thread(self, job: Job):
             with self.queue_condition:
-                if job_type == Job.JobType.THOUGHT:
-                    self._thought_thread_count -= 1
-                elif job_type == Job.JobType.ACTION:
-                    self._action_thread_count -= 1
-                elif job_type == Job.JobType.NOTIFICATION:
-                    self._notification_thread_count -= 1
+                if job.type in self.compatible_job_types:
+                    self._thread_counts[job.type] += 1
                 else:
-                    raise ValueError("Invalid JobType")
+                    raise ValueError(f"Invalid `{job.type.__class__.__name__}`")
             
-        def get_thread_count(self, job_type: Job.JobType):
+        def get_thread_count(self, job: Job):
             with self.queue_condition:
-                if job_type == Job.JobType.THOUGHT:
-                    return self._thought_thread_count
-                elif job_type == Job.JobType.ACTION:
-                    return self._action_thread_count
-                elif job_type == Job.JobType.NOTIFICATION:
-                    return self._notification_thread_count
+                if job.type in self.compatible_job_types:
+                    return self._thread_counts[job.type]
                 else:
-                    raise ValueError("Invalid JobType")
+                    raise ValueError(f"Invalid `{job.type.__class__.__name__}`")
             
         def get_all_threads_count(self):
             with self.queue_condition:
-                return self._thought_thread_count + self._action_thread_count + self._notification_thread_count
-    
+                return sum(self._thread_counts.values())
+            
+        def stop_thread(self, job: Job):
+            # with self.queue_condition:
+            if job.type in self.compatible_job_types:
+                self._thread_stops[job.type].set()
+            else:
+                raise ValueError(f"Invalid `{job.type.__class__.__name__}`")
+            
+        def check_stop_thread(self, job: Job):
+            # with self.queue_condition:
+            if job.type in self.compatible_job_types:
+                return self._thread_stops[job.type].is_set()
+            else:
+                raise ValueError(f"Invalid `{job.type.__class__.__name__}`")
+            
+        
     def __init__(self, max_threads: int):
         self.max_threads = max_threads
         self.thread_shelf = ThreadCafe.ThreadCounter()
@@ -64,18 +71,21 @@ class ThreadCafe:
         else:
             return self.thread_shelf.get_thread_count(job.type) == 0
     
+    # def check_thread_stop(self, job: Job) -> bool:
+        
+    
     def work(self, job: Job):
         try:
             # Execute the action's function
             if self.check_job_clearance(job):
-                self.thread_shelf.add_thread(job.type)
                 
                 # interrupt will stop all jobs of the same type
                 # TODO: interrupts should only stop jobs of the same class once classes are implemented
                 if job.priority == Job.Priority.INTERRUPT:
-                    
+                    self.thread_shelf.stop_thread(job)
                     
                 
+                self.thread_shelf.add_thread(job.type)
                 # job.execute_fn()
             # with self.queue_condition:
             # job.execute_fn()
